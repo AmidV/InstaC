b5b4e2713c7a8858be2652780813f0499885ef2f
Dmitriy Yegorov <yegorovd14@gmail.com>
2025-03-20 13:27:48 +0000

    Added and set up a Nativewind

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,8 +1,10 @@
-import { Text } from "react-native";
+import { Text, View } from "react-native";
 
 export default function FeedPage() {
     
     return (
-        <Text>All New Feed</Text>
+        <View className="bg-slate-300 flex-1 items-center justify-center m-5">
+            <Text className="text-3xl font-bold text-blue-500">All New Feed</Text>
+        </View>
     );
 }
